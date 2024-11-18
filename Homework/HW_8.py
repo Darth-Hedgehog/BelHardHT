b78df08f309203fae730ff571873f815f9b4fd8e
Darth-Hedgehog <filenotfound0@gmail.com>
2024-11-18 15:23:33 +0300

    Home_work_7 - Done

--- Homework/HW_8.py
+++ Homework/HW_8.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 
 class Transport():
     mileage: int = 0
@@ -8,13 +10,39 @@ class Transport():
         self.color = color
         self.mileage = 0
 
-
+    @abstractmethod
     def move(self, num_km):
         try:
-            if self.num_km > 0:
-                self.mileage += self.num_km
-        except:
-            
+            if num_km > 0:
+
+                self.mileage += num_km
+        except ValueError:
+            print(f"Расстояние должно быть положительным числом")
 
 
+class Car(Transport):
 
+    def __init__(self, brand: str, model: str, issue_year: str, color: str, engine_type: str):
+        super().__init__(brand, model, issue_year, color)
+        self.engine_type = engine_type
+
+    def move(self, num_km):
+        super().move(num_km)
+        return f"{self.brand} {self.model} ({self.color} - {self.issue_year}) проехала {self.mileage} километров"
+
+
+class Airplane(Transport):
+
+    def __init__(self, brand: str, model: str, issue_year: str, color: str, lifting_capacity: int):
+        super().__init__(brand, model, issue_year, color)
+        self.lifting_capacity = lifting_capacity
+
+    def move(self, num_km):
+        super().move(num_km)
+        return f"{self.brand} {self.model} ({self.color} - {self.issue_year}) пролетел {self.mileage} километров"
+
+jiguli_its_a_beer = Car('Jiguli', '5', 1967, 'Cherry', 'DVS')
+broiler_ne_747 = Airplane('Kurara', 'Peven', 2023, 'vletel iz pushki v stenu', 13)
+
+print(jiguli_its_a_beer.move(2167))
+print(broiler_ne_747.move(0.013))
